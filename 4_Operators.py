29630ad3d60605c6d3cb8daf0364f3158a25d0bd
Ritika <rkjane333@gmail.com>
2024-04-16 22:46:51 +0530

    First Commit

---
+++ 4_Operators.py
@@ -0,0 +1,25 @@
+# Operators
+print(5+6)
+print(15-6)
+print(15*6)
+print(15/6)
+print(15//6)   #floor
+print(5%3)     #modulus
+print(5**3)    #expontial
+
+
+# EXERCISE-1:CREATE a CALCULATOR
+'''Create a calculator capable of performing 
+addition, subtraction, multiplication and division
+operations on two numbers. Your program should format the output in 
+a readable manner!'''
+
+a=10
+b=4
+print("The addition of two number is:",a+b)
+print("The subtraction of two number is:",a-b)
+print("The multiplication of two number is:",a*b)
+print("The division of two number is:",a/b)
+print("The modulus of two number is:",a%b)
+
+
---
+++ 5_Typecasting.py
@@ -0,0 +1,21 @@
+# TypeCasting :The conversion of one data type into the other data type is
+# known as type casting in python or type conversion in python.
+
+a="1"
+b="2"
+print(a+b)  #String 
+
+#String to int 
+print(int(a)+int(b))
+
+#TYPE-
+# Explicit Conversion 
+# Implicit Conversion 
+
+# Implicit Conversion 
+c=1.9
+d=8
+print(c+d)  #float + int = float
+print(type(c))
+print(type(d))
+print(type(c+d))
