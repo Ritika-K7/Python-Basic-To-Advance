caab0e21ae97b0b3fcc3c2b15aaf38d305d0a849
Ritika <rkjane333@gmail.com>
2024-04-18 23:37:44 +0530

    String Operation

---
+++ 8_StringSlice&Operation.py
@@ -0,0 +1,28 @@
+#Slicing - [start index:end index]
+names="Ritika,Priya"
+print(names[0:6])  # o/p : Ritika
+
+#length of String - len()
+names="Ritika,Priya"
+print(len(names))
+
+
+fruit="Mango"
+Mangolen=len(fruit)
+print(Mangolen)
+print(fruit[0:4])
+print(fruit[1:4])
+print(fruit[:4])  #Take default start as zer0
+print(fruit[:])   #Take len of Mango
+print(fruit[0:-3])
+print(fruit[-3:-1])
+
+#Quick Quiz 
+nm="Harry"
+print(nm[-4:-2]) # O/p : ar  
+
+# ----------->
+# -5 -4 -3 -2 -1
+#  H  a  r  r  y
+ 
+
