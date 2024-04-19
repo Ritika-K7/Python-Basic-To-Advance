7c899b6b215902b4d2e0c3b452c7d7ef8d42e6ec
Ritika <rkjane333@gmail.com>
2024-04-19 23:36:22 +0530

    String methods

---
+++ 9_StringMethod.py
@@ -0,0 +1,32 @@
+#Strings are immutable - strings can not be changed ,string methods operats on your 
+#string and provide new string
+
+a="Ritika"
+print(len(a))
+
+#UPPER CASE -upper()
+print(a.upper())
+
+# Lower case -lower()
+print(a.lower())
+
+#To remove trailing charater(**not starting char(Eg.!!hello)) -rstrip("")
+b="Hello!!!!"
+print(b.rstrip("!"))
+
+#replace()- Replace all string by another string
+c="John"
+print(c.replace("J","d"))
+print(c.replace("John","Sliver"))
+
+#split() - split the string into list where ever space is there.
+d="Today is the silver Moon."
+print(d.split(" "))
+
+#Capitalize()
+# The capitalize() method turns only the first character of the string to uppercase 
+# and the rest other characters of the string are turned to lowercase
+heading="introduction to js"
+print(heading.capitalize())
+heading2="intro tO pD" #by mistake capital - O & D
+print(heading2.capitalize())
