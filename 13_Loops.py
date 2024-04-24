922d4f4534e308dc62e8459ff442056efeef1cc1
Ritika <rkjane333@gmail.com>
2024-04-24 22:48:28 +0530

    Loops

---
+++ 13_Loops.py
@@ -0,0 +1,12 @@
+# String
+name='Abhishek'
+for i in name:
+    print(i)
+
+# List   
+colors=['green','red','blue','yellow']
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+
