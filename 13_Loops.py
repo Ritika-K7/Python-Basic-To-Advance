fe7cbac9eb7042fdb8011bf1f760488adc360175
Ritika <rkjane333@gmail.com>
2024-05-12 21:13:12 +0530

    for loop

--- 13_Loops.py
+++ 13_Loops.py
@@ -10,3 +10,12 @@ for color in colors:
     for i in color:
         print(i)
 
+#range(start,end) start=0(default) ,end = not included
+for k in range(5):
+    print(k)
+
+for k in range(1,9):
+    print(k)    
+
+for k in range(1,12,3):   #start=1,end=11,jump=3
+    print(k)    
