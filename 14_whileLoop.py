425e028f9ea3346af732482034d95366d256b68c
Ritika <rkjane333@gmail.com>
2024-05-12 21:41:41 +0530

    while loop

---
+++ 14_whileLoop.py
@@ -0,0 +1,32 @@
+#while loop
+i=0
+while(i<3):
+    print(i)
+    i=i+1
+
+  
+# i=int(input("Enter number"))
+# while(i<=38):
+#     i=int(input("Enter the number"))
+#     print (i)
+
+# print("Done with the loop")    
+
+
+#Decremented while-loop
+count=5
+while(count>0):
+    print(count)
+    count=count-1
+
+#Else with while loop
+count=5
+while(count>0):
+    print(count)
+    count=count-1
+else:
+    print("I am inside while loop")
+
+''' NOTE :Ther is" NO Do-While loop" in python exist .
+To achive same functionality we can use statements 
+like break,continue  '''
