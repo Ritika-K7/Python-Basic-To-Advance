f565a7d6ea9067c68a2b44d777f33320956dd204
Ritika <rkjane333@gmail.com>
2024-05-21 23:12:09 +0530

    String reversed

---
+++ 110_StringReversal.py
@@ -0,0 +1,12 @@
+# Write a Python program that takes a string as input and prints the string in reverse order.
+
+s="Apple"
+l=len(s)
+for i in range(-1,-6,-1):
+    print(s[i])
+
+
+        
+    
+
+
