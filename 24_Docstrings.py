b1ce497358449bd73732b67ec7e9fdaa8434cbc1
Ritika <rkjane333@gmail.com>
2024-06-03 23:23:57 +0530

    docstring

---
+++ 24_Docstrings.py
@@ -0,0 +1,19 @@
+# Python docstrings are the string literals that appear right after the definition of a 
+# function, method, class, or module.
+
+def  square(n):
+    '''Takes in a numbern,returns the
+    square of n'''
+    print(n**2)
+
+square(5)
+print(square.__doc__)
+
+# **IMP:Docstring are not ignore by python interpreter.
+
+# Python Comments v/s Docstring
+# Comments are description that helps the programmers to better 
+# understand the intent of the program.They are completely ingnored by the python interpreter.
+#
+# Python docstring are strings used after the definition od a function ,method,class or midule .
+# They are use to document our code.
